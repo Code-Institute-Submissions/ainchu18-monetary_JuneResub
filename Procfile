ce40893587943297fd9313d4d4f76d3aa525c26e
ainchu18 <christianfordmacamchu18@gmail.com>
2022-04-27 12:58:43 +0000

    disabled collectstatic in heroku and 1st deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn monetary.wsgi:application
--- monetary/settings.py
+++ monetary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-t_d)6a5xgdnb5+1wwp)k28@xmf(jd8+@m3vnjhki!!1_^ue@j#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['monetary-v2.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -104,12 +104,17 @@ WSGI_APPLICATION = 'monetary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
